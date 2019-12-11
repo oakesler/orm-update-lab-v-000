5561ecd716d25a9dd6ff03a55bc43aeeac5a5736
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 19:25:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,8 @@ class Student
   end
 end
   
-  def self.create(name:, grade:)
-    student = Student.new(self.name, self.grade)
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
