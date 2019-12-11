83b90690ed015d9776cb31ce14112eba1b65c7e3
Olivia Kesler <oakesler@protonmail.ch>
2019-12-11 18:52:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -27,4 +27,49 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    #sql = <<-SQL
+      #INSERT INTO students (name, grade)
+      #VALUES (?, ?)
+    #SQL
+ 
+    #DB[:conn].execute(sql, self.name, self.grade)
+    #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+   def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  
+   def self.new_from_db(row)
+    new_student = self.new(row[0], row[1], row[2])
+    #new_student.id = row[0]
+    #new_student.name =  row[1]
+    #new_student.grade = row[2]
+    new_student
+  end
+  
+  def update
+    sql = "UPDATE songs SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
